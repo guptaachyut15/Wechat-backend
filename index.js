8561cd81231815823cf179503b17cc1163d4312e
Achyut Gupta <guptaachyut15@gmail.com>
2023-12-31 22:45:42 +0530

    chec

--- index.js
+++ index.js
@@ -5,39 +5,39 @@ const server = http.createServer(app);
 const { CORS_ORIGINS } = require("./config");
 
 const { Server } = require("socket.io");
-const allowedOrigins = CORS_ORIGINS.split(",");
-const io = new Server(server, {
-  cors: {
-    origin: allowedOrigins,
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
+// const allowedOrigins = CORS_ORIGINS.split(",");
+// const io = new Server(server, {
+//   cors: {
+//     origin: allowedOrigins,
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   },
+// });
 
-const users = {};
+// const users = {};
 
-server.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.end("On the backend page for chat app,go back!");
 });
 
-io.on("connection", (socket) => {
-  socket.on("user-joined", (name) => {
-    console.log(`${name} joined`);
-    users[socket.id] = name;
-    socket.broadcast.emit("user-joined", name);
-  });
+// io.on("connection", (socket) => {
+//   socket.on("user-joined", (name) => {
+//     console.log(`${name} joined`);
+//     users[socket.id] = name;
+//     socket.broadcast.emit("user-joined", name);
+//   });
 
-  socket.on("send", (msg) => {
-    console.log(`${users[socket.id]} messaged`);
-    socket.broadcast.emit("send", { name: users[socket.id], msg: msg });
-  });
+//   socket.on("send", (msg) => {
+//     console.log(`${users[socket.id]} messaged`);
+//     socket.broadcast.emit("send", { name: users[socket.id], msg: msg });
+//   });
 
-  socket.on("disconnect", () => {
-    const disconnectedUser = users[socket.id];
-    socket.broadcast.emit("user-left", disconnectedUser);
-    delete users[socket.id];
-  });
-});
+//   socket.on("disconnect", () => {
+//     const disconnectedUser = users[socket.id];
+//     socket.broadcast.emit("user-left", disconnectedUser);
+//     delete users[socket.id];
+//   });
+// });
 
 server.listen(8080, () => {
   console.log(`Listening on port 8080`);
